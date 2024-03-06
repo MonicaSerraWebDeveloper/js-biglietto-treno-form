07eb78a5163870a481c83649a1a6f59a70d07b3a
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-06 15:44:41 +0100

    feat: add the event listener click when the button is clicked and creted two variables to create the price of the ticket

--- js/script.js
+++ js/script.js
@@ -5,11 +5,21 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 
-// Creaiamo la variabile per il pulsante
-// Creaiamo l'evento di ascolto al click per il pulsante
+// Creiamo la variabile per il pulsante
+const buttonCalculatorTicket = document.querySelector('.calculator');
+
+// Creiamo l'evento di ascolto al click per il pulsante
+buttonCalculatorTicket.addEventListener('click', function () {
     // Creiamo la variabile per l'input Km
+    const userKm = parseInt (document.querySelector('#trip-distance').value);
     // Creiamo la variabile per definire il numero della moltiplicazione 0.21
-    // Creaiamo il calcolo matematico tra la variabile Km e il numero 0.21
+    const numberForCalculation = 0.21;
+    // Creiamo il calcolo matematico tra la variabile Km e il numero 0.21
+    const regularPrice = userKm * numberForCalculation;
+
+});
+    
+    // Creiamo il calcolo matematico tra la variabile Km e il numero 0.21
     // Creiamo la variabile per l'input Age
     // Creiamo lo sconto 20%
     // Creiamo lo sconto 40% 
