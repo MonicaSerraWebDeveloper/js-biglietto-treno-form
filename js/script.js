77510c8ace94185ff9172edbf32e9f9fc4df459a
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-06 19:42:07 +0100

    feat: add the dynamic part to the milestone 2 using the manipulation to the style with js

--- js/script.js
+++ js/script.js
@@ -24,8 +24,34 @@ buttonCalculatorTicket.addEventListener('click', function () {  // Creiamo l'eve
         totalPrice = discountForty
     }
 
+    let offerMessageUser = 'Biglietto standard'; // Milestone 2 Creare la variabile del messaggio
+
+    if (userAge < 18) { // Milestone 2 Creare la condizione per mostrare un messaggio
+        offerMessageUser = 'Sconto del 20%'
+    } else if (userAge >= 65) {
+        offerMessageUser = 'Sconto del 40%'
+    }
+
+    document.querySelector('.offer-user').innerHTML = offerMessageUser; // Milestone 2 stampare il messaggio in HTML
+
     const roundedPrice = parseFloat(totalPrice.toFixed(2)); // Arrotondiamo i decimali ad un massimo di due cifre
 
-    document.querySelector('.message-for-user').innerHTML = `Il prezzo del tuo biglietto è: € ${roundedPrice}`  // Stampiamo il risultato finale 
+    document.querySelector('.message-for-user').innerHTML = `€ ${roundedPrice}`  // Stampiamo il risultato finale 
 });
+
+// Milestone 2
+
+buttonCalculatorTicket.addEventListener('click', function () { // Creare una variabile per il nome inserito in input
+    const userName = document.querySelector('#user-name').value;
+    document.querySelector('.result-user').innerHTML = userName // Inseriamo lo userName dove desideriamo farlo comparire
     
+    const containerTicketResult = document.querySelector('.ticket-result-container'); // Milestone 2 far comparire il risultato del biglietto attraverso style
+    containerTicketResult.style.display = 'block'
+});
+
+
+
+
+
+
+
