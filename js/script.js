5429d40227fa5cf1d31e099f1a764ee33c19a171
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-06 14:32:01 +0100

    feat: add all the sequence in js as a comment

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+
+// Creaiamo la variabile per il pulsante
+// Creaiamo l'evento di ascolto al click per il pulsante
+    // Creiamo la variabile per l'input Km
+    // Creiamo la variabile per definire il numero della moltiplicazione 0.21
+    // Creaiamo il calcolo matematico tra la variabile Km e il numero 0.21
+    // Creiamo la variabile per l'input Age
+    // Creiamo lo sconto 20%
+    // Creiamo lo sconto 40% 
+    // Creiamo la condizione per gli under 18 e gli over 65
+    // Stampiamo il risultato finale 
