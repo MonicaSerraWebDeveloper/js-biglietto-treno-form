30e7ddc63937b9e4911461697ac96f6d3b034250
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-06 16:24:40 +0100

    feat: add rounded price applied with to fixed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,22 @@
-// - Il numero di chilometri da percorrere
-// - Età del passeggero
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// - va applicato uno sconto del 20% per i minorenni
-// - va applicato uno sconto del 40% per gli over 65.
+// Milestone 1
 
-const buttonCalculatorTicket = document.querySelector('.calculator'); // Creiamo la variabile per il pulsante
+const buttonCalculatorTicket = document.querySelector('.calculator');  // Creiamo la variabile per il pulsante
 
-buttonCalculatorTicket.addEventListener('click', function () { // Creiamo l'evento di ascolto al click per il pulsante
+buttonCalculatorTicket.addEventListener('click', function () {  // Creiamo l'evento di ascolto al click per il pulsante
    
     const userKm = parseInt (document.querySelector('#trip-distance').value);  // Creiamo la variabile per l'input Km
     
-    const numberForCalculation = 0.21;     // Creiamo la variabile per definire il numero della moltiplicazione 0.21
+    const numberForCalculation = 0.21;  // Creiamo la variabile per definire il numero della moltiplicazione 0.21
 
-    const regularPrice = userKm * numberForCalculation;     // Creiamo il calcolo matematico tra la variabile Km e il numero 0.21
+    const regularPrice = userKm * numberForCalculation;  // Creiamo il calcolo matematico tra la variabile Km e il numero 0.21
 
-    const userAge = parseInt (document.querySelector('#user-age').value);     // Creiamo la variabile per l'input Age
+    const userAge = parseInt (document.querySelector('#user-age').value);  // Creiamo la variabile per l'input Age
 
-    const discountTwenty = regularPrice - (regularPrice * 20 / 100);     // Creiamo lo sconto 20%
+    const discountTwenty = regularPrice - (regularPrice * 20 / 100);  // Creiamo lo sconto 20%
     
-    const discountForty = regularPrice - (regularPrice * 40 / 100);     // Creiamo lo sconto 40% 
+    const discountForty = regularPrice - (regularPrice * 40 / 100);  // Creiamo lo sconto 40% 
 
-    let totalPrice = regularPrice;     // Creiamo la condizione per gli under 18 e gli over 65
+    let totalPrice = regularPrice;  // Creiamo la condizione per gli under 18 e gli over 65
 
     if (userAge < 18) {
         totalPrice = discountTwenty
@@ -29,6 +24,9 @@ buttonCalculatorTicket.addEventListener('click', function () { // Creiamo l'even
         totalPrice = discountForty
     }
 
-    document.querySelector('.message-for-user').innerHTML = `Il prezzo del tuo biglietto è: ${totalPrice}`     // Stampiamo il risultato finale 
+    const roundedPrice = parseFloat(totalPrice.toFixed(2)); // Arrotondiamo i decimali ad un massimo di due cifre
+    console.log(roundedPrice);
+
+    document.querySelector('.message-for-user').innerHTML = `Il prezzo del tuo biglietto è: ${roundedPrice}`  // Stampiamo il risultato finale 
 });
     
